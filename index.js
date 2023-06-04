ce12d9c1812879587a0452e23ce055397661528a
ophill <philliplima142@gmail.com>
2023-06-04 20:42:23 +0100

    index correction commit

--- index.js
+++ index.js
@@ -90,8 +90,11 @@ function declareWinner(roomUniqueId) {
     }, 3000);
 }
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log('Listening on port', PORT);
 });
 
 function makeid(length) {
